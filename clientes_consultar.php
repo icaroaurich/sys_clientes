b9e07cb0ca6542194cb64f8751d2106f18336e62
icaroaurich <icaroaurich@hotmail.com>
2023-12-20 10:05:43 -0300

    Aplicado Bootstrap nos arquivos clientes_consultar e clientes_incluir

--- clientes_consultar.php
+++ clientes_consultar.php
@@ -3,30 +3,31 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css\style01.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Icaro CRUD PHP</title>
 </head>
 
 <body>
-    <h1>Tela de consulta clientes</h1>
+    <div class="p-1 mb-2 bg-primary text-white">
+        <h1 class="text-center">Tela de consultar clientes</h1>
+    </div>
 
-    <div class="div03">
-        <form action="index.html">
-            <button type="submit" style="background-color:red;">Sair</button></form></div>
-    <div class="div02">
-        <form action="clientes_consultar.php">
-            <button type="submit">Consultar clientes</button></form></div>
-    <div class="div02">
-        <form action="clientes_incluir.php">
-            <button type="submit">Incluir clientes</button></form></div>
+    <div class="btn-group mb-4" role="group">
+        <a href="index.html"             class="btn btn-danger"  aria-current="page">Sair</a>
+        <a href="clientes_consultar.php" class="btn btn-primary" aria-current="page">Consultar clientes</a>
+        <a href="clientes_incluir.php"   class="btn btn-primary" aria-current="page">Incluir clientes</a>
+    </div>
             
-    <form method="post">
-        Name: <input type="text" name="name"><br>
-            <input type="submit"></form>
+    <form method="post" class="fs-5">
+        <div class="input-group mb-4">
+            <label for="floatingInputGroup1">Nome do usuario: </label>
+            <input type="text" class="form-control" id="floatingInputGroup1" name="name" placeholder="Nome">
+        </div>
+    </form>
 
     <?php
         include('conexao.php');
-        echo $_POST['name'];
         if ($_POST['name'] != ''){
             $QueryGetClientes = "select id_cliente, nome_cliente, cpf_cliente from clientes where nome_cliente like '%".$_POST['name']."%';";
         }
@@ -35,30 +36,29 @@
         }
         $QueryGetClientesResult = mysqli_query($conexao,$QueryGetClientes);
 
-            $tabela = '<table border="1">';
-            $tabela .='<thead>';
-            $tabela .= '<tr>';
-            $tabela .= '<th>ID</th>'; //            ID
-            $tabela .= '<th>Nome</th>';//           Nome
-            $tabela .= '<th>CPF</th>';//            cpf
-            $tabela .= '<th>Excluir</th>';
-            $tabela .= '</tr>';
-            $tabela .='</thead>'; 
-            $tabela .='<tbody>';
+        $tabela = '<table class="table table-bordered table-striped border-primary">';
+        $tabela .='<thead>';
+        $tabela .= '<tr>';
+        $tabela .= '<th>ID</th>'; //            ID
+        $tabela .= '<th>Nome</th>';//           Nome
+        $tabela .= '<th>CPF</th>';//            cpf
+        $tabela .= '<th>Excluir</th>';
+        $tabela .= '</tr>';
+        $tabela .='</thead>'; 
+        $tabela .='<tbody>';
 
         while($row = mysqli_fetch_assoc($QueryGetClientesResult)) {
             $tabela .= '<tr>'; 
-            $tabela .= '<td><a href="clientes_alterar.php?id='.$row['id_cliente'].'">'.$row['id_cliente']. '</a></td>';
+            $tabela .= '<td><a class="btn btn-info" href="clientes_alterar.php?id='.$row['id_cliente'].'">'.$row['id_cliente']. '</a></td>';
             $tabela .= '<td>'.$row['nome_cliente'].'</td>'; //      Nome
             $tabela .= '<td>'.$row['cpf_cliente'].'</td>'; //       CPF
             $tabela .= '<td><a href="clientes_deletar_confirmar.php?id='.$row['id_cliente'].'"><img src="image\lixeira.ico"></a></td>';
             $tabela .= '</tr>';
-           }
+        }
 
         $tabela .='</tbody>'; 
         $tabela .= '</table>';
 
         echo $tabela;
     ?>
-
 </body>
--- clientes_incluir.php
+++ clientes_incluir.php
@@ -3,40 +3,52 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css\style01.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Icaro CRUD PHP</title>
 </head>
 
 <body>
-    <h1>Tela de consulta clientes</h1>
-    
-    <div class="div03">
-        <form action="index.html">
-            <button type="submit" style="background-color:red;">Sair</button></form></div>
-    <div class="div02">
-        <form action="clientes_consultar.php">
-            <button type="submit">Consultar clientes</button></form></div>
-    <div class="div02">
-        <form action="clientes_incluir.php">
-            <button type="submit">Incluir clientes</button></form></div>
-            
-    <form method="post">
-        Name: <input type="text" name="name" required><br>
-        cpf: <input type="text" name="cpf" required><br>
-            <input type="submit"></form>
+    <div class="p-1 mb-2 bg-primary text-white">
+        <h1 class="text-center">Tela de incluir clientes</h1>
+    </div>
+
+    <div class="btn-group mb-4" role="group">
+        <a href="index.html"             class="btn btn-danger"  aria-current="page">Sair</a>
+        <a href="clientes_consultar.php" class="btn btn-primary" aria-current="page">Consultar clientes</a>
+        <a href="clientes_incluir.php"   class="btn btn-primary" aria-current="page">Incluir clientes</a>
+    </div>
+
+    <form method="post" class="fs-5">
+        <div class="input-group mb-2">
+            <label for="floatingInputGroup1">Nome do usuario:</label>
+            <input type="text" class="form-control" id="floatingInputGroup1" name="name" placeholder="Nome" required>
+        </div>
+        <div class="input-group mb-2">
+            <label for="floatingInputGroup2">CPF do usuario:</label>
+            <input type="text" class="form-control" id="floatingInputGroup2" name="cpf" placeholder="CPF" required>
+        </div>
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <input class="btn btn-primary" type="submit" value="Incluir cliente" >
+        </div>
+    </form>
 
     <?php
         include('conexao.php');
-        if ($_POST['name'] == '' or $_POST['cpf'] == ''){echo "Conteudo invalido!";}
+        if ($_POST['name'] == '' or $_POST['cpf'] == ''){echo "
+                <div class='alert alert-warning' role='alert'>
+                    Algo de errado nao esta certo!
+                </div>
+            ";}
         else {
-            echo $_POST['name'];
-            echo "<br>";
-            echo $_POST['cpf'];
-            echo "<br>";
 
             $query = "insert into clientes (nome_cliente,cpf_cliente) values ('".$_POST['name']."','".$_POST['cpf']."');";
             $QueryGetClientesResult = mysqli_query($conexao,$query);
-            if ($QueryGetClientesResult == 1){echo "Cliente inserido com sucesso!";};
+            if ($QueryGetClientesResult == 1){echo "
+                    <div class='alert alert-success' role='alert'>
+                        Cliente inserido com sucesso!
+                    </div>
+                ";};
         }
 
     ?>
